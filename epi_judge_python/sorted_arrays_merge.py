fbefd5e6002805004a573555d4f87c9168a4a847
Kashyap <kashyapmaduri@outlook.com>
2024-04-01 23:34:15 -0500

    11-1 done

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,29 @@
+import heapq
+from heapq import heappush, heappop
 from typing import List
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    curr_heap = []
+    result = []
+    for (i, l) in enumerate(sorted_arrays):
+        if len(l) == 0: continue
+        heapq.heappush(curr_heap, (l[0], i, 0))
+
+    while len(curr_heap) > 0:
+        val, list_idx, idx_in_list = heappop(curr_heap)
+        result.append(val)
+        idx_in_list += 1
+
+        if idx_in_list < len(sorted_arrays[list_idx]):
+            heappush(curr_heap,
+                    (sorted_arrays[list_idx][idx_in_list],
+                        list_idx,
+                        idx_in_list))
+
+    return result
 
 
 if __name__ == '__main__':
